bb0c821f24e75b56adcdf51ec0034641b65fb4b2
David Little <drabbytux@gmail.com>
2020-08-07 09:37:22 -0400

    completed word count with log file

--- module-1/word-count.cpp
+++ module-1/word-count.cpp
@@ -1,15 +1,20 @@
 /**
- * 1. Write code that will use cin.get() to read characters in one by one until a newline is encountered. Report how many words there were in the input, how long the longest word was, and the largest number of vowels in a word (work on one of these at a time!). You may not store any of the characters but you can have other variables that store information about them as you go along.
-
-2. Think about all potential cases. What types of input could break your program? What if someone presses enter right away? What if there’s only one word? What if there are many words? What if there are multiple spaces between words? etc… Make sure it all works.
-
-For this portion, copy and paste the log of your console interactions in a new log.txt file (to be included in the PR). This log will demonstrate the good coverage of your program. You can manually divide and annotate test cases if it gives more clarity. 
+ * Word Count
  * 
+ * It takes in user input and does several calculations on the words entered.
+ * @author David Little
+ * @date Aug 6, 2020
  */
 #include <iostream>
 
 using namespace std;
 
+/**
+ * isVowel checks if the charachter passed is a vowel 
+ * 
+ * @param char the character passed
+ * @returns bool true if a vowel, false if not.
+ **/
 bool isVowel(char str){
   char x = tolower(str);
   if(x=='a'||x=='e'||x=='i'||x=='o'||x=='u')
@@ -17,10 +22,15 @@ bool isVowel(char str){
   else 
     return false;
 }
+
+/**
+ * getUserInput takes in user input and does a variety of calculations on cin.get() until the line return is used. It then outputs using cout the calculations done.
+ * 
+ **/
 void getUserInput(){
 
   int charItem, charTally = 0, vowelTally = 0, longestWord = 0, shortestWord = 0, numberOfWords = 0, longestVowelTally = 0;
-  bool hitEnter = false;
+  bool hitEnter = false, wordsContainNonAlphaChars = false;
 
   while( !hitEnter ){
 
@@ -30,10 +40,16 @@ void getUserInput(){
 
       charTally++;
 
+      // Tally the vowels
       if( isVowel(charItem) )
         vowelTally++;
 
+      // Flag non-alpha characters
+      if( !isalpha(charItem) )
+        wordsContainNonAlphaChars = true;
+
     } else {
+
       // We've hit a space. Did our new word make the cut?
       if( charTally != 0 ) {
 
@@ -51,22 +67,28 @@ void getUserInput(){
         vowelTally = 0;
       }
     }
-    
+    // Line return was hit, our work here is done.
     if( charItem == 10 )
       hitEnter = true;
   }
-
-  cout << "There were " << numberOfWords << " words.\n";
-  cout << "The longest word was " << longestWord << " characters.\n";
-  cout << "The shortest word was " << shortestWord << " characters.\n";
-  cout << "The most vowels used in a word were " << longestVowelTally << " vowels.\n";
+    if( numberOfWords ){
+      cout << "\nThere were " << numberOfWords << " words in total.\n";
+      cout << "\t- The longest word was " << longestWord << " character(s).\n";
+      cout << "\t- The shortest word was " << shortestWord << " character(s).\n";
+      cout << "\t- The most vowels used in a word were " << longestVowelTally << " vowel(s).\n";
+      if( wordsContainNonAlphaChars )
+        cout << "FYI - Some words contained non-alphabetic characters but were included.\n";
+    } else { 
+      cout << "\nYou didn't enter any words. That wasn't any fun.\n";
+    }
 }
 
 int main(){
+  char response;
 
   cout << "Enter some words, please: ";
   getUserInput();
-  
-  cout << "Great work!\n";
+  cout << "\n";
+
   return 0;
 }
