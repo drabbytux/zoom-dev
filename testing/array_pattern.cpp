5a81427f279532c122ed2975e28395ddc3570400
David Little <drabbytux@gmail.com>
2020-08-10 07:26:23 -0400

    Array patten for design testing

---
+++ testing/array_pattern.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+  /*
+  block a (normal)
+  #----
+  -##--
+  array{{1,0,0,0,0}, {0,1,1,0,0}}
+
+  block b (normal flipped horizontal)
+  ----#
+  --##-
+  array{{0,0,0,0,1}, {0,0,1,1,0}}
+
+  block c (normal flipped vertical)
+  -##--
+  #----
+  array{{0,1,1,0,0}, {1,0,0,0,0}}
+
+  block d ( normal flipped horizontal and vertical)
+  --##-
+  ----#
+  array{{0,0,1,1,0}, {0,0,0,0,1}}
+
+
+  ////////// Combined:
+  array{ {1,0,0,0,0}, {0,1,1,0,0}, {0,0,0,0,1}, {0,0,1,1,0}, {0,1,1,0,0}, {1,0,0,0,0}, {0,0,1,1,0}, {0,0,0,0,1} }
+
+  */
+
+
+  return 0;
+}
