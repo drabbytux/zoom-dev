b2490d355bb68626cfd9f7bf92ba12a50e3baf34
David Little <drabbytux@gmail.com>
2020-08-11 07:47:30 -0400

    testing array pattern

--- testing/array_pattern.cpp
+++ testing/array_pattern.cpp
@@ -5,25 +5,31 @@ using namespace std;
 int main() {
 
   /*
-  block a (normal)
+block a (normal)
+  array level 0 containing a number of arrays at level 1
   #----
   -##--
   array{{1,0,0,0,0}, {0,1,1,0,0}}
+>>>> Logic? good to go (normal).
 
-  block b (normal flipped horizontal)
+block b (normal flipped horizontal)
   ----#
   --##-
   array{{0,0,0,0,1}, {0,0,1,1,0}}
+>>>> Logic? Reverse order of each level 1 array
 
-  block c (normal flipped vertical)
+block c (normal flipped vertical)
   -##--
   #----
   array{{0,1,1,0,0}, {1,0,0,0,0}}
+>>>> Logic? Reverse order of Level 0 array
 
-  block d ( normal flipped horizontal and vertical)
+block d ( normal flipped horizontal and vertical)
   --##-
   ----#
   array{{0,0,1,1,0}, {0,0,0,0,1}}
+>>>> Logic? Reverse order of Level 1 array, reverse order of level 0 array
+(take block b and reverse level 0)
 
 
   ////////// Combined:
