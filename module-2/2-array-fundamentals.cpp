97fbeae6e653b38cc837013152631ef4260a5be7
David Little <drabbytux@gmail.com>
2020-08-11 08:32:14 -0400

    Clean up of array fundamentals

---
+++ module-2/2-array-fundamentals.cpp
@@ -0,0 +1,12 @@
+/**
+ * Write a C++ program that defines a constant (fixed) array of at least 10 characters. This must be an array of char, not a string type. Loop through the array and count how many characters are uppercase. Demonstrate that your code works correctly in main().
+ * */
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+  return 0;
+}
