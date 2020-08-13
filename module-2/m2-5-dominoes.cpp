651c4a27cdf9089505d15e9eb48035cb1d399529
David Little <drabbytux@gmail.com>
2020-08-13 07:50:26 -0400

    Last touch ups

--- module-2/m2-5-dominoes.cpp
+++ module-2/m2-5-dominoes.cpp
@@ -1,14 +1,6 @@
 /**
  * @file m2-5-dominoes
  * 
- * Define a struct that represents information about a single domino, keeping in mind that a single domino has two sets of 0 to 6 dots. You must decide what should be included and what the data types of the members will be.
-
-In main(), declare an array of your domino structs that will store a double-six set of dominoes (Links to an external site.). (If you happen to know of more than one way to create arrays, make sure you are using a static array.) Write the necessary code using loop(s) needed to populate the array with all the dominoes in a set. The dominoes can be in any order.
-
-Write code to sort the dominoes in a new order. You don’t need to write a function to do this, but may if you wish to.
-
-Write code that prints the contents of the sorted dominoes array using asterisks (*) and any other characters needed, displaying them similar to how real dominoes would be laid out. When the program is run, it must be clear that your array has been sorted in a different order than it was created in.
-
  * @author David Little
  * @date August 12, 2020
  * @version 1.0
@@ -19,17 +11,24 @@ Write code that prints the contents of the sorted dominoes array using asterisks
 
 using namespace std;
 
+/**
+ *  domino struct, simply a top number and bottom number
+ **/
 struct domino {
   int topNumber;
   int bottomNumber;
 };
 
-
-domino *populateDominoDeck(domino dominoDeck[], int TOTAL_DOMINOS ) {
+/**
+ *  populateDominoDeck takes a blank array of domino tiles and adds the needed dots or "pips" to each.
+ * 
+ * @param domino dominoDeck array, int total number of dominos
+ * @returns pointer dominoDeck
+ **/
+domino *populateDominoDeck( domino dominoDeck[], int TOTAL_DOMINOS ) {
   int dominoInst = 0;
   for(int bottomNum = 0; bottomNum < 7; bottomNum++){
     for(int topNum = 0; topNum <= bottomNum; topNum++){
-      // cout <<  dominoInst << "=" << topNum << " | " << bottomNum << "\n";
       dominoDeck[dominoInst].topNumber = topNum;
       dominoDeck[dominoInst].bottomNumber = bottomNum;
       dominoInst++;
@@ -38,10 +37,35 @@ domino *populateDominoDeck(domino dominoDeck[], int TOTAL_DOMINOS ) {
   return dominoDeck;
 }
 
+/**
+ *  *randomizeDeck takes in the deck of domnios and shuffles them up
+ * @param domino dominoDeck array, int total number of dominos
+ * @returns pointer dominoDeck
+ **/
+domino *randomizeDeck( domino dominoDeck[], int TOTAL_DOMINOS ){
+  int numberOfShuffles = TOTAL_DOMINOS;
+  int x = 0;
+  domino pickedDomnio;
+  int randomElement = 0;
 
+  while( x < numberOfShuffles ){
+    randomElement =  rand() % TOTAL_DOMINOS; 
+    pickedDomnio = dominoDeck[randomElement];
+    dominoDeck[randomElement]= dominoDeck[x];
+    dominoDeck[x] = pickedDomnio;
+    x++;
+  }
+  return dominoDeck;
+}
+
+
+/**
+ *  getRowPips will send you the current row of dots or "pips" as a string.
+ * @param int row number, int current number on the top/bottom portion of domnio
+ * @returns string of pips
+ **/
 string getRowPips(int row, int num){
   string returnStr = "";
-  // cout << "row " << row << ", number " << num << "\n";
   returnStr += "|";
   switch (row){
     case 1 :  // -- first row
@@ -66,25 +90,24 @@ string getRowPips(int row, int num){
       // ***** pip 3
       returnStr += ( num >= 2 ) ? "*" : " "; // single character
     break;
-
-    
   }
   returnStr += "|";
   return returnStr;
 }
 
 
+/**
+ *  *randomizeDeck takes in the deck of domnios and processes each design needed for each domino
+ * @param domino dominoDeck array, int total number of dominos
+ * @returns string of all the domino designs
+ **/
 string getDominoDesign( domino dominoDeck[], int TOTAL_DOMINOS ){
   int x=0;
   string dominoDesign = "\0";
-
   while(x < TOTAL_DOMINOS){
-
     dominoDesign += " ___\n";
-
     int half=1;
     while( half <= 2 ){
-
       int row=1;
       while( row <= 3 ){
         int numberOnHalfDomino =  (half==1) ? dominoDeck[x].topNumber : dominoDeck[x].bottomNumber;
@@ -92,11 +115,9 @@ string getDominoDesign( domino dominoDeck[], int TOTAL_DOMINOS ){
         dominoDesign += "\n";
         row++;
       }
-
       half++;
       dominoDesign += (half==2)? "|- -|\n": "";
     }
-
     dominoDesign += "////\n";
     x++;
   }
@@ -111,14 +132,9 @@ int main(){
   domino dominoDeck[TOTAL_DOMINOS];
   populateDominoDeck(dominoDeck, TOTAL_DOMINOS );
 
-  int i = 0;
-  while( i< TOTAL_DOMINOS ) {
-    // cout <<  dominoDeck[i].topNumber << " | " << dominoDeck[i].bottomNumber << "\n";
-    i++;
-  }
+  randomizeDeck( dominoDeck, TOTAL_DOMINOS );
+  cout << getDominoDesign( dominoDeck, TOTAL_DOMINOS );
 
-  cout <<  getDominoDesign( dominoDeck, TOTAL_DOMINOS );
-  // cout <<  dominoDeck[8].topNumber << " | " << dominoDeck[8].bottomNumber << "\n";
 
   return 0;
 }
