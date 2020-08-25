592ac53cc78458b06fb3474468a47b3f6913c60f
David Little <drabbytux@gmail.com>
2020-08-25 07:54:30 -0400

    quick java tests

---
+++ testing/Ball.java
@@ -0,0 +1,6 @@
+package testing;
+
+public class Ball {
+  int x;
+  int y;
+}
---
+++ testing/BallTester.java
@@ -0,0 +1,10 @@
+class BallTester {
+  public static void main(String[] args)
+  {
+    //Ball[] ballArray = new Ball[3];
+    //ballArray[0].x = 5;
+    //ballArray[0].y = 10;
+    System.out.println("OK!!!");
+    // System.out.println(ballArray[0] + " " + ballArray[0]);
+  }
+}
