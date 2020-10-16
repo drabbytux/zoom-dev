d93d6755894cf673d74838793ea68d359a08a9dd
David Little <drabbytux@gmail.com>
2020-10-16 08:28:48 -0400

    Part 1 done

---
+++ ruby/ascii_art.rb
@@ -0,0 +1,17 @@
+# Ascii art p1
+#
+# David Little
+# Oct 15 2020
+# Write a Ruby program that prompts a person to enter a number, and then outputs a triangle of asterisks of that height and length.
+
+def output(original_number, index=1)
+  return if index > original_number
+  i = 1..index
+  i.each {|n| print "*" }
+  print "\n"
+  output(original_number, index+1)
+end
+
+print "Enter a number: "
+num = gets.chomp.to_i
+output(num)
