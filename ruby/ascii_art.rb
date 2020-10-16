da465abeff5fe9ad61aa54b845861ae8a0eb971a
David Little <drabbytux@gmail.com>
2020-10-16 08:43:02 -0400

    Part 3 TREE style complete

--- ruby/ascii_art.rb
+++ ruby/ascii_art.rb
@@ -3,15 +3,19 @@
 # David Little
 # Oct 15 2020
 # Write a Ruby program that prompts a person to enter a number, and then outputs a triangle of asterisks of that height and length.
+#  *
+# * *
+#* * *
+
 
 def output(original_number, index=1)
   return if index > original_number
 
   space = 1..original_number - index
   space.each {|s| print "\s"}
-  
+
   i = 1..index
-  i.each { |n| print "*" }
+  i.each { |n| print "* " }
   print "\n"
   output(original_number, index+1)
 end
